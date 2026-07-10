98e692ab421558d846fd495eec7a5002921e50ee
barisddemirci <barisddemirci@gmail.com>
2026-07-10 11:58:28 +0300

    Güncellemeler Trelloda belirtildi (Infrastructure-AppDbContext)

---
+++ ERMS.Domain/Entities/RequestComment.cs
@@ -0,0 +1,15 @@
+﻿namespace ERMS.Domain.Entities;
+
+public class RequestComment
+{
+    public int RequestCommentId { get; set; }              
+
+    public int RequestId { get; set; }              
+    public Request Request { get; set; } = null!;
+
+    public int AuthorId { get; set; }                 
+    public User Author { get; set; } = null!;
+
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+}
---
+++ ERMS.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Azure.Core;
+using ERMS.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ERMS.Infrastructure.Persistence;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<ERMS.Domain.Entities.Department> Departments => Set<ERMS.Domain.Entities.Department>();
+    public DbSet<ERMS.Domain.Entities.Request> Requests => Set<ERMS.Domain.Entities.Request>();
+    public DbSet<RequestType> RequestTypes => Set<RequestType>();
+    public DbSet<Approval> Approvals => Set<Approval>();
+    public DbSet<RequestComment> RequestComments => Set<RequestComment>();
+    public DbSet<RequestHistory> RequestHistories => Set<RequestHistory>();
+}
